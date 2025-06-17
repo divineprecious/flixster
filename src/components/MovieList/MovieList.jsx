93fa96ad20f92cff177d8fe0e8b709749f51fa38
Divine Precious-Esue <divinepreciousesue@gmail.com>
2025-06-17 13:40:08 -0700

    Removed console.log

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -51,8 +51,6 @@ export default function MovieList({search, sort}) {
         setMovies([]);
     }, [search, sort]);
 
-
-    console.log(movies);
     return (
     <>
     <div className="movie-list">
